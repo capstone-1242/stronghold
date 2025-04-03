88f892e8a94f4908a341393b8387d1ac3d31d315
Sydney <SydneyKampjes@gmail.com>
2025-04-03 12:39:46 -0600

    Filter button components

--- resources/views/Components/role-button.blade.php
+++ resources/views/Components/role-button.blade.php
@@ -1,34 +1,22 @@
 <div class="role-btn-container">
-    <a href="/testimonials?tags%5B%5D=1&submit=Apply+Filters" class="role-btn">
-        <img src="{{ asset('images/military.svg') }}" alt="Military icon">
-        Military
-    </a>
-    <a href="/testimonials?tags%5B%5D=6&submit=Apply+Filters" class="role-btn">
-        <img src="{{ asset('images/police.svg') }}" alt="Police icon">
-        Police
-    </a>
-    <a href="/testimonials?tags%5B%5D=4&submit=Apply+Filters" class="role-btn">
-        <img src="{{ asset('images/firefighter.svg') }}" alt="Firefighter icon">
-        Firefighter
-    </a>
-    <a href="/testimonials?tags%5B%5D=7&submit=Apply+Filters" class="role-btn">
-        <img src="{{ asset('images/paramedic.svg') }}" alt="Paramedic icon">
-        Paramedic
-    </a>
-    <a href="/testimonials?tags%5B%5D=8&submit=Apply+Filters" class="role-btn">
-        <img src="{{ asset('images/hospital.svg') }}" alt="Hospital icon">
-        Hospital
-    </a>
-    <a href="/testimonials?tags%5B%5D=5&submit=Apply+Filters" class="role-btn">
-        <img src="{{ asset('images/dispatcher.svg') }}" alt="Dispatcher icon">
-        Dispatcher
-    </a>
-    <a href="/testimonials?tags%5B%5D=9&submit=Apply+Filters" class="role-btn">
-        <img src="{{ asset('images/families.svg') }}" alt="Families icon">
-        Families
-    </a>
+    @foreach ($tags as $tag)
+        <a href="{{ url('/testimonials?tags[]=' . $tag->id . '&submit=Apply+Filters') }}" class="role-btn">
+            @php
+                $componentName = strtolower($tag->name);
+            @endphp
+
+            @if (view()->exists("components.$componentName"))
+                @component("components.$componentName", ['fill' => 'white', 'stroke' => 'white']) @endcomponent
+            @else
+                <x-all fill="white"/>
+            @endif
+
+            {{ $tag->name }}
+        </a>
+    @endforeach
+
     <a href="/testimonials" class="role-btn">
-        <img src="{{ asset('images/all.svg') }}" alt="All Resources icon">
+        <x-all fill="white"/>
         All Resources
     </a>
 </div>
